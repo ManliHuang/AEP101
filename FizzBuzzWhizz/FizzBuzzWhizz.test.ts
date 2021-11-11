41af80cd614e985d2713ef3c9eedc5742435eb8e
ManliHuang <manli.huang@thoughtworks.com>
2021-11-11 20:28:19 +0800

    test: 8th case when number is dividable by 3, 5, 7

--- FizzBuzzWhizz/FizzBuzzWhizz.test.ts
+++ FizzBuzzWhizz/FizzBuzzWhizz.test.ts
@@ -4,25 +4,25 @@ describe('The number is neither dividable by nor including 3, 5, 7', () => {
     test('should return 1 when queueing number is 1 which is not dividable by 3, 5, 7 and excludes 3, 5, 7', () => {
         const fizzBuzzWhizz = new FizzBuzzWhizz(1);
         expect(fizzBuzzWhizz.report()).toEqual("1");
-    })
-})
+    });
+});
 
 describe('The number is only dividable by one target number and excludes 3, 5, 7', () => {
     test('should return Fizz when queueing number is 6 which is only dividable by 3 and excludes 3, 5, 7', () => {
         const fizzBuzzWhizz = new FizzBuzzWhizz(6);
         expect(fizzBuzzWhizz.report()).toEqual("Fizz");
-    })
+    });
 
     test('should return Buzz when queueing number is 5 which is only dividable by 5 and excludes 3, 5, 7', () => {
         const fizzBuzzWhizz = new FizzBuzzWhizz(5);
         expect(fizzBuzzWhizz.report()).toEqual("Buzz");
-    })
+    });
 
     test('should return Whizz when queueing number is 7 which is only dividable by 7 and excludes 3, 5, 7', () => {
         const fizzBuzzWhizz = new FizzBuzzWhizz(7);
         expect(fizzBuzzWhizz.report()).toEqual("Whizz");
-    })
-})
+    });
+});
 
 describe('The number is dividable by more than one target number and excludes 3, 5, 7', () => {
     test('should return FizzBuzz when queueing number is 60 which is dividable by 3 and 5', () => {
@@ -33,10 +33,16 @@ describe('The number is dividable by more than one target number and excludes 3,
     test('should return FizzWhizz when queueing number is 21 which is dividable by 3, 7', () => {
         const fizzBuzzWhizz = new FizzBuzzWhizz(21);
         expect(fizzBuzzWhizz.report()).toEqual('FizzWhizz');
-    })
+    });
 
     test('should return BuzzWhizz when queueing number is 140 which is dividable by 5, 7', () => {
         const fizzBuzzWhizz = new FizzBuzzWhizz(140);
         expect(fizzBuzzWhizz.report()).toEqual('BuzzWhizz');
+    });
+
+    test('should return FizzBuzzWhizz when queueing number is 210 which is dividable by 3, 5, 7', () => {
+        const fizzBuzzWhizz = new FizzBuzzWhizz(210);
+        expect(fizzBuzzWhizz.report()).toEqual('FizzBuzzWhizz');
     })
+
 })
