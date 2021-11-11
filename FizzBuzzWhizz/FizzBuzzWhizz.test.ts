fe6e0062efc8a1108a15d83cce6f98fe364b0481
ManliHuang <manli.huang@thoughtworks.com>
2021-11-11 16:41:18 +0800

    test: add forth case the number is only dividable by 7 and classify tests

--- FizzBuzzWhizz/FizzBuzzWhizz.test.ts
+++ FizzBuzzWhizz/FizzBuzzWhizz.test.ts
@@ -1,11 +1,13 @@
 import FizzBuzzWhizz from "./FizzBuzzWhizz";
 
-describe('FizzBuzzWhizz', () => {
+describe('The number is neither dividable by nor including 3, 5, 7', () => {
     test('should return 1 when queueing number is 1 which is not dividable by 3, 5, 7 and excludes 3, 5, 7', () => {
         const fizzBuzzWhizz = new FizzBuzzWhizz(1);
         expect(fizzBuzzWhizz.report()).toEqual("1")
     })
+})
 
+describe('The number is only dividable by one target number and excludes 3, 5, 7', () => {
     test('should return Fizz when queueing number is 6 which is only dividable by 3 and excludes 3, 5, 7', () => {
         const fizzBuzzWhizz = new FizzBuzzWhizz(6);
         expect(fizzBuzzWhizz.report()).toEqual("Fizz")
@@ -14,5 +16,10 @@ describe('FizzBuzzWhizz', () => {
     test('should return Buzz when queueing number is 5 which is only dividable by 5 and excludes 3, 5, 7', () => {
         const fizzBuzzWhizz = new FizzBuzzWhizz(5);
         expect(fizzBuzzWhizz.report()).toEqual("Buzz")
+    })
+
+    test('should return Whizz when queueing number is 7 which is only dividable by 7 and excludes 3, 5, 7', () => {
+        const fizzBuzzWhizz = new FizzBuzzWhizz(7);
+        expect(fizzBuzzWhizz.report()).toEqual("Whizz")
     })
 })
