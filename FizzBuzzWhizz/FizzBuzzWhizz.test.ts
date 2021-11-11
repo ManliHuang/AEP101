14db5320fd0f49ad36ac4efd4a98c2c1bfa29f76
ManliHuang <manli.huang@thoughtworks.com>
2021-11-11 10:40:02 +0800

    fix test: convert expect result to be string

--- FizzBuzzWhizz/FizzBuzzWhizz.test.ts
+++ FizzBuzzWhizz/FizzBuzzWhizz.test.ts
@@ -1,4 +1,6 @@
+import FizzBuzzWhizz from "./FizzBuzzWhizz";
+
 test('should return 1 when queueing number 1 which is not dividable by 3, 5, 7 and excludes 3, 5, 7', () => {
     const fizzBuzzWhizz = new FizzBuzzWhizz(1);
-    expect(fizzBuzzWhizz.report).toEqual(1)
+    expect(fizzBuzzWhizz.report()).toEqual("1")
 })
