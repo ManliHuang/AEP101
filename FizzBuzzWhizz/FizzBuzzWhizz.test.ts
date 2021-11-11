2978249c20ed740e03c4f7e3ed1308e042672a78
ManliHuang <manli.huang@thoughtworks.com>
2021-11-11 10:43:35 +0800

    test: 2nd case in tasking

--- FizzBuzzWhizz/FizzBuzzWhizz.test.ts
+++ FizzBuzzWhizz/FizzBuzzWhizz.test.ts
@@ -1,6 +1,11 @@
 import FizzBuzzWhizz from "./FizzBuzzWhizz";
 
-test('should return 1 when queueing number 1 which is not dividable by 3, 5, 7 and excludes 3, 5, 7', () => {
+test('should return 1 when queueing number is 1 which is not dividable by 3, 5, 7 and excludes 3, 5, 7', () => {
     const fizzBuzzWhizz = new FizzBuzzWhizz(1);
     expect(fizzBuzzWhizz.report()).toEqual("1")
+})
+
+test('should return Fizz when queueing number is 6 which is only dividable by 3 and excludes 3, 5, 7', () => {
+    const fizzBuzzWhizz = new FizzBuzzWhizz(6);
+    expect(fizzBuzzWhizz.report()).toEqual("Fizz")
 })
