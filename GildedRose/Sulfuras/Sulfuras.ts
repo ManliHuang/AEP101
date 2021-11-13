35fdb088d9b0e3cc33ba088d86904f587098768c
ManliHuang <manli.huang@thoughtworks.com>
2021-11-13 19:40:18 +0800

    production: should increase 1 when one day passed if past expiration

--- GildedRose/Sulfuras/Sulfuras.ts
+++ GildedRose/Sulfuras/Sulfuras.ts
@@ -1,7 +1,7 @@
 export default class Sulfuras {
     private _sellIn: number;
     private _quality: number;
-    private _qualityChangeScale: number
+    private _qualityChangeScale: number = 1
 
     constructor(sellIn: number, quality: number) {
         this._quality = quality;
@@ -18,7 +18,7 @@ export default class Sulfuras {
 
     public daysPassed(days: number = 1) {
         this._sellIn -= days;
-        this._qualityChangeScale = 1;
         this._quality += this._qualityChangeScale;
+        this._quality = Math.min(this._quality, 50)
     }
 }
