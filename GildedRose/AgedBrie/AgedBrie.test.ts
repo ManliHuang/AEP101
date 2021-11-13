d7ae519013da4b7e3cbeb23d44b91c81357f3952
ManliHuang <manli.huang@thoughtworks.com>
2021-11-13 19:13:45 +0800

    test: quality should not be bigger than 50

--- GildedRose/AgedBrie/AgedBrie.test.ts
+++ GildedRose/AgedBrie/AgedBrie.test.ts
@@ -6,6 +6,12 @@ describe('Aged Brie', () => {
         agedBrie.daysPassed();
         expect(agedBrie.sellIn).toEqual(0);
         expect(agedBrie.quality).toEqual(50);
+    })
 
+    test('SellIn should be 0 and Quality should be 50 when one day passes given Quality is 50 and SellIn is 1', () =>{
+        const agedBrie = new AgedBrie(1, 50);
+        agedBrie.daysPassed();
+        expect(agedBrie.sellIn).toEqual(0);
+        expect(agedBrie.quality).toEqual(50);
     })
 })
