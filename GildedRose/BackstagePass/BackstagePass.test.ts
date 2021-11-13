682e486fb5592daacab0969e6ba9900d6e0400f5
ManliHuang <manli.huang@thoughtworks.com>
2021-11-13 23:00:41 +0800

    test: quality increase 2 given sellin among 5 to 10

--- GildedRose/BackstagePass/BackstagePass.test.ts
+++ GildedRose/BackstagePass/BackstagePass.test.ts
@@ -7,4 +7,11 @@ describe('Backstage Pass', () => {
         expect(backstagePass.sellIn).toEqual(10);
         expect(backstagePass.quality).toEqual(2);
     })
+
+    test('SellIn should be 9 and Quality should be 3 when one day passes given Quality is 1 and SellIn is 10', () => {
+        const backstagePass = new BackstagePass(10, 1);
+        backstagePass.daysPassed();
+        expect(backstagePass.sellIn).toEqual(9);
+        expect(backstagePass.quality).toEqual(3);
+    })
 })
