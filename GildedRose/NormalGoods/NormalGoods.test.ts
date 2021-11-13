a780d4db47524d5015054a3000299fd65879a633
ManliHuang <manli.huang@thoughtworks.com>
2021-11-13 15:47:27 +0800

    test: 2nd case of normal goods - Quality cannot be lower than 0

--- GildedRose/NormalGoods/NormalGoods.test.ts
+++ GildedRose/NormalGoods/NormalGoods.test.ts
@@ -3,9 +3,15 @@ import NormalGoods from "./NormalGoods";
 describe('Normal Goods', () => {
     test('SellIn and Quality should be 0 when one day passes given Quality and SellIn are both 1', () => {
         const normalGoods = new NormalGoods(1, 1);
-        normalGoods.daysPassed(1);
+        normalGoods.daysPassed();
         expect(normalGoods.sellIn).toEqual(0);
         expect(normalGoods.quality).toEqual(0);
+    })
 
+    test('SellIn should be -1 and Quality should be 0 when one day passes given Quality and SellIn are both 0', () => {
+        const normalGoods = new NormalGoods(0, 0);
+        normalGoods.daysPassed();
+        expect(normalGoods.sellIn).toEqual(-1);
+        expect(normalGoods.quality).toEqual(0);
     })
 })
