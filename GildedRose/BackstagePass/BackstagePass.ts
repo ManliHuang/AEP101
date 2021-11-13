66adf1da325ab4dd838ab5b224383579787a2dff
ManliHuang <manli.huang@thoughtworks.com>
2021-11-13 23:05:33 +0800

    production: quality increase 2 given sellin among 5 to 10

--- GildedRose/BackstagePass/BackstagePass.ts
+++ GildedRose/BackstagePass/BackstagePass.ts
@@ -2,6 +2,9 @@ import Goods from "../Goods/Goods";
 
 export default class BackstagePass extends Goods {
     public daysPassed(days:number = 1) {
+        if (this.sellIn <= 10 && this.sellIn > 5) {
+            this.qualityChangeScale = 2
+        }
         this.sellIn -= days;
         this.quality += this.qualityChangeScale;
     };
