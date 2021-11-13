b86107c03d36f002b7952a8f9a241e98316ed0b9
ManliHuang <manli.huang@thoughtworks.com>
2021-11-13 18:44:38 +0800

    production:  quality should increase 1 when one day passes during expiration date

--- GildedRose/AgedBrie/AgedBrie.test.ts
+++ GildedRose/AgedBrie/AgedBrie.test.ts
@@ -1,3 +1,5 @@
+import AgedBrie from "./AgedBrie";
+
 describe('Aged Brie', () => {
     test('SellIn should be 0 and Quality should be 50 when one day passes given Quality is 49 and SellIn is 1', () =>{
         const agedBrie = new AgedBrie(1, 49);
---
+++ GildedRose/AgedBrie/AgedBrie.ts
@@ -0,0 +1,25 @@
+export default class {
+    private _sellIn: number;
+    private _quality: number;
+    private _qualityMinusScale: number;
+
+    constructor(sellIn: number, quality: number) {
+        this._sellIn = sellIn;
+        this._quality = quality;
+    }
+
+    public get sellIn() {
+        return this._sellIn;
+    }
+
+    public get quality() {
+        return this._quality
+    }
+
+    public daysPassed(days: number = 1) {
+        this._sellIn -= days;
+
+        this._qualityMinusScale = 1;
+        this._quality += this._qualityMinusScale
+    }
+}
