61f165c58153da77efe3af58e1bc434e99b3cb30
ManliHuang <manli.huang@thoughtworks.com>
2021-11-13 16:27:08 +0800

    test: quality cannot be lower than 0

--- GildedRose/NormalGoods/NormalGoods.test.ts
+++ GildedRose/NormalGoods/NormalGoods.test.ts
@@ -21,4 +21,11 @@ describe('Normal Goods', () => {
         expect(normalGoods.sellIn).toEqual(-2);
         expect(normalGoods.quality).toEqual(0);
     })
+
+    test('SellIn should be -2 and Quality should be 0 when one day passes given the Quality is 1 and SellIn is -1', () => {
+        const normalGoods = new NormalGoods(-1, 1);
+        normalGoods.daysPassed();
+        expect(normalGoods.sellIn).toEqual(-2);
+        expect(normalGoods.quality).toEqual(0);
+    })
 })
