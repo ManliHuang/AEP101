8b3509eb0907243c2dc0dccc753b2b5c4b3b99b7
ManliHuang <manli.huang@thoughtworks.com>
2021-11-13 16:24:43 +0800

    production: Quality minus 2 given past expiration date

--- GildedRose/NormalGoods/NormalGoods.ts
+++ GildedRose/NormalGoods/NormalGoods.ts
@@ -1,6 +1,7 @@
 export default class NormalGoods {
     private _sellIn: number;
     private _quality: number;
+    private _qualityMinusScale: number = 1;
 
     constructor(sellIn: number, quality: number) {
         this._sellIn = sellIn;
@@ -18,8 +19,12 @@ export default class NormalGoods {
     public daysPassed(days: number = 1) {
         this._sellIn -= days
 
+        if (this._sellIn < 0) {
+            this._qualityMinusScale = 2;
+        }
+
         if (this._quality > 0) {
-            this._quality -= days;
+            this._quality -= days * this._qualityMinusScale;
         }
     }
 }
