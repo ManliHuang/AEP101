7b1be190a4a4f132713b59a9bdbbba747e86c0b1
ManliHuang <manli.huang@thoughtworks.com>
2021-11-13 16:21:26 +0800

    test: Quality minus 2 given past expiration date

--- GildedRose/NormalGoods/NormalGoods.test.ts
+++ GildedRose/NormalGoods/NormalGoods.test.ts
@@ -14,4 +14,11 @@ describe('Normal Goods', () => {
         expect(normalGoods.sellIn).toEqual(-1);
         expect(normalGoods.quality).toEqual(0);
     })
+
+    test('SellIn should be -2 and Quality should be 0 when one day passes given the Quality is 2 and SellIn is -1', () => {
+        const normalGoods = new NormalGoods(-1, 2);
+        normalGoods.daysPassed();
+        expect(normalGoods.sellIn).toEqual(-2);
+        expect(normalGoods.quality).toEqual(0);
+    })
 })
